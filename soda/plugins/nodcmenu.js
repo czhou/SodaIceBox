3d9905f297dcdf16c11a0d30d45eac076adbf122
Trampus <trampus@gmail.com>
2011-07-13 17:49:15 -0700

    windows

---
+++ soda/plugins/nodcmenu.js
@@ -0,0 +1,12 @@
+var menu = document.getElementById("dcmenu");
+var dcbox = document.getElementById("dcboxbody");
+
+if (dcbox != null) {
+   dcbox.style.display = "none";
+}
+
+if (menu != null) {
+   menu.style.display = "none";
+}
+
+return 0;
